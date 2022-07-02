413f3421464e5c2c5b814f628bb18359dde81ba4
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-02 16:28:16 +0300

    Sidebar auto closing on mobile devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,14 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false)
+    }
+  }
+
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
@@ -19,7 +26,7 @@ const Sidebar = () => {
     { activeMenu && (<>
       <div className='flex justify-between
       item-center '>
-        <Link to="/" onClick={() => setActiveMenu(false)}
+        <Link to="/" onClick={handleCloseSideBar}
         className='items-center gap-3 ml-3
         mt-4 flex text-xl font-extrabold 
         tracking-tight dark:text-white
@@ -51,7 +58,8 @@ const Sidebar = () => {
             <NavLink
               to={`/${link.name}`}
               key={link.name}
-              onClick={() => {}}
+              // modified for moboile devices
+              onClick={handleCloseSideBar}
               className={({ isActive }) => 
             isActive ? activeLink : normalLink}
             >
