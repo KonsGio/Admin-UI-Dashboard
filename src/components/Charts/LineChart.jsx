c2bbc228ecfc9a33fe416a90667e4a956d143529
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-03 16:59:58 +0300

    Line chart complete and color changes from theme selection

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective,
+SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries} from '@syncfusion/ej2-react-charts'
+
+import { lineCustomSeries,LinePrimaryYAxis,LinePrimaryXAxis } from '../../data/dummy'
+import  { useStateContext } from '../../contexts/ContextProvider';
+
 
 const LineChart = () => {
+  const { currentMode,currentColor } = useStateContext();
   return (
-    <div>LineChart</div>
+    <ChartComponent
+    id='line-chart'
+    height='420px'
+    // X and Y axis data 
+    primaryXAxis={LinePrimaryXAxis}
+    primaryYAxis={LinePrimaryYAxis}
+    chartArea={{border:{width:0}}}
+    tooltip={{enable:true}}
+    background={currentMode === 'Dark' ? currentColor : '#FFF'}
+    >
+      <Inject services = {[LineSeries,DateTime,Legend,Tooltip]}/>
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item,index)=>
+         <SeriesDirective ket={index}{...item}/>)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
