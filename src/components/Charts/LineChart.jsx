356e7bb547d5954e6182c5d81d0339e4b4b8e752
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-03 17:32:13 +0300

    Line chart white background

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -3,11 +3,9 @@ import { ChartComponent, SeriesCollectionDirective,
 SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries} from '@syncfusion/ej2-react-charts'
 
 import { lineCustomSeries,LinePrimaryYAxis,LinePrimaryXAxis } from '../../data/dummy'
-import  { useStateContext } from '../../contexts/ContextProvider';
 
 
 const LineChart = () => {
-  const { currentMode,currentColor } = useStateContext();
   return (
     <ChartComponent
     id='line-chart'
@@ -17,8 +15,6 @@ const LineChart = () => {
     primaryYAxis={LinePrimaryYAxis}
     chartArea={{border:{width:0}}}
     tooltip={{enable:true}}
-    background={currentMode === 'Dark' ? currentColor : '#FFF'}
-    legendSettings={{ background: 'white' }}
     >
       <Inject services = {[LineSeries,DateTime,Legend,Tooltip]}/>
       <SeriesCollectionDirective>
