da036ddf864d4d58216d295bd045bbd93d112a6b
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-03 12:26:39 +0300

    Launching Employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    // Orders menu tab
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Employees" />
+      <GridComponent
+      // pupulate data coming from dummy 
+      dataSource={employeesData}
+      // pagination
+      allowPaging
+      // sorting for categories
+      allowSorting
+      toolbar={['Search']}
+      width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item,index) => (
+          <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        {/* Next button for pagination */}
+        <Inject services ={[Page, Search, Toolbar]}/>
+      </GridComponent>
+    </div>
   )
 }
 
