97d5631419d8916ce963d8b9e0ac39990cff4bb6
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-03 16:27:53 +0300

    Dark mode working except on divs background

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import './App.css'
 import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const {activeMenu,themeSettings,setThemeSettings,currentColor} = useStateContext();
+  const {activeMenu,themeSettings,setThemeSettings,currentMode,currentColor} = useStateContext();
 
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
           <div className='fixed right-4 bottom-4 ' style={{ zIndex: '1000'}}>
@@ -42,8 +42,10 @@ const App = () => {
             </div>
           )}
           <div className={
-            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 
-            'md:ml-72' : 'flex-2'}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+             ${activeMenu 
+              ? 'md:ml-72' 
+              : 'flex-2'}`
           }>
           <div className='fixed md:static  
           bg-main-bg dark:bg-main-dark-bg 
