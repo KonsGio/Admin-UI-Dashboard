cc5a4c4256d0e98338d3359b73d67dc085c63bbf
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-03 21:24:05 +0300

    Owend by Shoppy.com

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 Developed by Kostas Giovanitsas
+      © 2022 Re-Engineered by Kostas Giovanitsas off Shoppy.com
     </p>
   </div>
 )
