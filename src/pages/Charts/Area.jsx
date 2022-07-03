64f770865b2597d0c1e0c2fa11e397a0592a2b24
KonsGio <konstantinos.giovanitsas@yahoo.com>
2022-07-03 17:31:59 +0300

    Area chart white background

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -3,10 +3,8 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Dat
 
 import { Header } from '../../components';
 import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
 
 const Area = () => {
-  const { currentMode,currentColor } = useStateContext();
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white  rounded-3xl">
@@ -17,8 +15,6 @@ const Area = () => {
           primaryXAxis={areaPrimaryXAxis}
           primaryYAxis={areaPrimaryYAxis}
           chartArea={{ border: { width: 0 } }}
-          background={currentMode === 'Dark' ? currentColor : '#fff'}
-          legendSettings={{ background: 'white' }}
         >
           <Inject services={[SplineAreaSeries, DateTime, Legend]} />
           <SeriesCollectionDirective>
